24c29a0e3218a9eafe10d7b7f7ec712846ff8fa3
overtrue <anzhengchao@gmail.com>
2022-06-05 22:37:25 +0800

    courses

--- components/CourseCard.vue
+++ components/CourseCard.vue
@@ -3,10 +3,10 @@
     <nuxt-link :to="course.url"><img :src="course.image_url" class="bg-white w-full object-cover" :alt="course.name" /></nuxt-link>
     <div class="col-span-2 space-y-4 flex flex-col">
       <nuxt-link :to="course.url" class="font-bold">{{ course.name }}</nuxt-link>
-      <div>
-        来自 <nuxt-link :to="course.author_url">{{ course.author }}</nuxt-link>
+      <div class="text-sm text-gray-500">
+        来自 <nuxt-link :to="course.author_url" class="text-indigo-900 dark:text-indigo-400">{{ course.author }}</nuxt-link>
       </div>
-      <nuxt-link :to="course.url" class="block w-full flex-1">
+      <nuxt-link :to="course.url" class="block w-full flex-1 text-gray-500">
         {{ course.introduction }}
       </nuxt-link>
     </div>

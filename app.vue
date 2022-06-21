57f06e6c680e7d527fee8cf1f317de2bfcad25bf
脆皮丶 <cuiqg@foxmail.com>
2022-06-21 18:10:31 +0800

    style: hero 几个字没有上边距

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col h-full text-gray-700 dark:text-gray-400">
     <TheNavbar />
-    <div class="container mx-auto px-6 flex-1">
+    <div class="container mx-auto p-6 flex-1">
       <NuxtPage />
     </div>
     <TheFooter />
